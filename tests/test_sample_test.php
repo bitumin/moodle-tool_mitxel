3fa7e5ca337d62f62506ca2a945394e95aad16c2
Mitxel Moriana <mitxel@tresipunt.com>
2018-08-01 15:20:38 +0200

    added sample test case

---
+++ tests/test_sample_test.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+class tool_mitxel_sample_testcase extends advanced_testcase {
+
+    public function setUp() {
+        $this->resetAfterTest(true);
+    }
+
+    /**
+     * Test sample test.
+     */
+    public function test_sample_test() {
+        $this->assertEquals(1, 1);
+    }
+
+}
